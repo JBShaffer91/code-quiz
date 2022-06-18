7b61656dc4bbf75767dc8d85baa854f3a343eac2
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-18 00:48:08 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 // Business Logic
 function check() {
+
+  let question1 = document.quiz.question1.value;
+  let question2 = document.quiz.question2.value;
+  let question3 = document.quiz.question3.value;
+  let question4 = document.quiz.question4.value;
+  let question5 = document.quiz.question5.value;
+  
   document.getElementById("after_submit").style.visibility="visible";
 }
 
