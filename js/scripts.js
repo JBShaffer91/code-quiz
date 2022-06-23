9563661b56ad2f3efeaf4a4b84f0598fa91f9e15
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-22 18:22:33 -0700

    add radio button format, add js points system, changed background

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,61 @@
-const startButton = document.getElementById('start-btn');
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+function check() {
 
-startButton.addEventListener('click', startQuiz)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-
-startButton.classList.add('hide')
-currentQuestionIndex = 0
-
-
-function startQuiz() {
-  consol.log('Started')
-  startButton.classList.add('hide')
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+if ("question1" == "l1") {
+    correct = 1;
 }
-
-function setNextQuestion() {
-  resetState()
-  showQuestion(currentQuestionIndex)
+if ("question1" == "l2") {
+    correct = 2;
 }
-
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-
-function resetState() {
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild
-    (answerButtonsElement.firstChild)
+if ("question1" == "l3") {
+    correct = 3;
   }
+
+if ("question2" == "l1") {
+  correct = 1;
+}
+if ("question2" == "l2") {
+  correct = 2;
+}
+if ("question2" == "l3") {
+  correct = 3;
 }
 
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  nextButton.classList.remove('hide')
-  startButton.innerText = 'Restart'
-  startButton.classList.remove('hide')
+if ("question3" == "l1") {
+  correct = 1;
+}
+if ("question3" == "l2") {
+  correct = 2;
+}
+if ("question3" == "l3") {
+  correct = 3;
 }
 
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add(correct)
-  } else {
-    element.classList.add('wrong')
+if ("question4" == "l1") {
+  correct = 1;
+}
+if ("question4" == "l2") {
+  correct = 2;
+}
+if ("question4" == "l3") {
+  correct = 3;
+}
+
+if ("question5" == "l1") {
+  correct = 1;
+}
+if ("question5" == "l2") {
+  correct = 2;
+}
+if ("question5" == "l3") {
+  correct = 3;
+}
+
+let message = ["JavaScript", "Python", "Solidity"];
+let range;
+
+
+
+window.onload = function() {
+
   }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
-// const questions = {
-    question: 'What is your favorite color?'
-    answers: 
-      {text: 'Red'}
-      {text: 'Blue'}
-      {text: 'Black'}
-
-    question: 'What is your favorite movie genre?'
-    answers:
-      {text: 'Comedy'}
-      {text: 'Horror'}
-      {text: 'Sci-Fi'}
-
-    question: 'What is your favorite music genre?'
-    answers:
-      {text: 'Punk Rock'}
-      {text: 'Metal'}
-      {text: 'Hip Hop'}
-
-    question: 'What is your favorite city?'
-    answers: 
-      {text: 'Los Angeles, CA'}
-      {text: 'Portland, OR'}
-      {text: 'New York City, NY'}
-
-    question: 'Who is your favorite superhero?'
-    answers:
-      {text: 'Spider-Man'}
-      {text: 'Thor'}
-      {text: 'Hulk'}
 }
