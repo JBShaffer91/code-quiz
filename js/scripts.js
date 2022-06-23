1c60b2bdbb8ce66124a2b7c5f7f3ce2bc7aca281
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-22 18:25:54 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,11 @@
 function check() {
+const question1 = document.getElementById("mc1");
+const question2 = document.getElementById("mc2");
+const question3 = document.getElementById("mc3");
+const question4 = document.getElementById("mc4");
+const question5 = document.getElementById("mc5");
 
-if ("question1" == "l1") {
-    correct = 1;
-}
-if ("question1" == "l2") {
-    correct = 2;
-}
-if ("question1" == "l3") {
-    correct = 3;
-  }
 
-if ("question2" == "l1") {
-  correct = 1;
-}
-if ("question2" == "l2") {
-  correct = 2;
-}
-if ("question2" == "l3") {
-  correct = 3;
-}
-
-if ("question3" == "l1") {
-  correct = 1;
-}
-if ("question3" == "l2") {
-  correct = 2;
-}
-if ("question3" == "l3") {
-  correct = 3;
-}
-
-if ("question4" == "l1") {
-  correct = 1;
-}
-if ("question4" == "l2") {
-  correct = 2;
-}
-if ("question4" == "l3") {
-  correct = 3;
-}
-
-if ("question5" == "l1") {
-  correct = 1;
-}
-if ("question5" == "l2") {
-  correct = 2;
-}
-if ("question5" == "l3") {
-  correct = 3;
 }
 
 let message = ["JavaScript", "Python", "Solidity"];
