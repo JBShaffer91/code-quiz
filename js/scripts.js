684b6abfd9111489e7b7548518c73043c82b539f
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-24 19:45:39 -0700

    add branching to question form

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
-function check() {
-const question1 = document.getElementById("mc1");
-const question2 = document.getElementById("mc2");
-const question3 = document.getElementById("mc3");
-const question4 = document.getElementById("mc4");
-const question5 = document.getElementById("mc5");
+// Business Logic
 
+window.onload = function check() {
 
+let question1 = document.getElementById("mc1");
+let question2 = document.getElementById("mc2");
+let question3 = document.getElementById("mc3");
+let question4 = document.getElementById("mc4");
+let question5 = document.getElementById("mc5");
+
+let jMessage.style.display = "none";
+let rbMessage.style.display = "none";
+let cMessage.style.display = "none";
+
+const form = document.querySelector("form");
+
+form.onsubmit = function(event) {
+  event.preventDefault():
+
+  const mc1 = parseInt(document.querySelector("input#mc1").value);
+  const mc2 = parseInt(document.querySelector("input#mc2").value);
+  const mc3 = parseInt(document.querySelector("input#mc3").value);
+  const mc4 = parseInt(document.querySelector("input#mc4").value);
+  const mc5 = parseInt(document.querySelector("input#mc5").value);
+
+  if (mc1 >= 1)
 }
 
-let message = ["JavaScript", "Python", "Solidity"];
-let range;
-
-
-
-window.onload = function() {
-
-  }
 }
