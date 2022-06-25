ca99683b75ec397d6668d82b28e8bbc8aadd8562
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-25 00:52:35 -0700

    add refresh button to form

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,11 @@
-// Business Logic
-
-window.onload = function check() {
-
-let question1 = document.getElementById("mc1");
-let question2 = document.getElementById("mc2");
-let question3 = document.getElementById("mc3");
-let question4 = document.getElementById("mc4");
-let question5 = document.getElementById("mc5");
-
-let jMessage.style.display = "none";
-let rbMessage.style.display = "none";
-let cMessage.style.display = "none";
-
-const form = document.querySelector("form");
-
-form.onsubmit = function(event) {
-  event.preventDefault():
-
-  const mc1 = parseInt(document.querySelector("input#mc1").value);
-  const mc2 = parseInt(document.querySelector("input#mc2").value);
-  const mc3 = parseInt(document.querySelector("input#mc3").value);
-  const mc4 = parseInt(document.querySelector("input#mc4").value);
-  const mc5 = parseInt(document.querySelector("input#mc5").value);
-
-  if (mc1 >= 1)
-}
-
-}
+// User Interface Logic
+window.onload = function() {
+  //start of click event handler for language reveal
+  document.querySelector("show").onclick = function() {
+    document.getElementById()
+  }
+  //start of click event handler for redo button
+  document.querySelector("redo").onclick = function() {
+   window.location.reload()
+  }
+};
