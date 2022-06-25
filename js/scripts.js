203f3f72c827753eb5eec1c38e20a87005e6af1f
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-25 01:47:08 -0700

    almost get Show me button to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
+// Business Interface
+
+
 // User Interface Logic
 window.onload = function() {
-  //start of click event handler for language reveal
-  document.querySelector("show").onclick = function() {
-    document.getElementById()
-  }
-  //start of click event handler for redo button
-  document.querySelector("redo").onclick = function() {
-   window.location.reload()
-  }
+ function check() {
+   document.getElementById("after_submit").style.visibility="visible";
+ }
 };
