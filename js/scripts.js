b45211621c8fd6b6272011d3a2fc3b33ab94152c
Justin Shaffer <justinbshaffer91@gmail.com>
2022-06-25 23:30:01 -0700

    add window.onload funtion

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,18 @@
 
 // User Interface Logic
 window.onload = function() {
+
+  let quiz = document.getElementById("quiz");
+
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+
+    const q5Input = document.getElementById("q5Input").value;
+
+    document.querySelector("span#q5Input").radio = q1Input;
+  }
+
+
  function check() {
    document.getElementById("after_submit").style.visibility="visible";
  }
